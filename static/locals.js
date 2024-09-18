401f5c785bb25fd026444fe8a3ea7a9d2f87c1e8
Nicko The Drako PaxPax <drako@paxpax.me>
2024-09-18 08:49:55 -0600

    New post Oracle EPS Wallets

---
+++ static/locals.js
@@ -0,0 +1,13 @@
+var sc_project=12975911;                                   
+var sc_invisible=0;                                         
+var sc_security="d1376fb0";                                 
+var scJsHost = "https://";                                  
+document.write("<script type='text/javascript' src='" + scJsHost +
+"statcounter.com/counter/counter.js'></script>");
+
+document.write('<noscript><div class="statcounter"><a title="web counter" \
+href="https://statcounter.com/" target="_blank"><img                      \
+class="statcounter"                                                       \
+src="https://c.statcounter.com/12975911/0/d1376fb0/0/"                    \
+alt="web counter"                                                         \
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>');
